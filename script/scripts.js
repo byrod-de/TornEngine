04d33d5df5f3149ef65de7be1f866cb00356c8e2
byrod <77072122+byrod-de@users.noreply.github.com>
2021-03-18 10:10:00 +0100

    Update scripts.js
    - added faction totals
    - formatting
    - added success rate

--- script/scripts.js
+++ script/scripts.js
@@ -24,7 +24,7 @@
 				} else {
 					
 					if (jsonData.hasOwnProperty('crimes') && jsonData.hasOwnProperty('members')){
-						printAlert('Success', 'API Call successful!');
+						printAlert('Success', 'The API Call successful, find the results below.');
 						parseCrimes(jsonData['crimes'], 'output', jsonData['members']);
 					} else {
 						printAlert('Warning', 'Ask your faction leader for faction API permissions.');
@@ -32,40 +32,35 @@
 				}
 				
 			} else {
-				printAlert('#chedded', 'Torn API not available.');
+				printAlert('#chedded', 'Torn API is currently not available.');
 			}
 		}
 		request.send();
 	}
 	
-	function printAlert(alertType, alertText) {
-		var alertClass;
-		if (alertType === 'Error') { alertClass = 'alert-danger' };
-		if (alertType === 'Success') { alertClass = 'alert-success' };
-		if (alertType === 'Info') { alertClass = 'alert-info' };
-		if (alertType === 'Warning') { alertClass = 'alert-warning' };
-		if (alertType === '#chedded') { alertClass = 'alert-danger' };
-		
-		document.getElementById('alert').innerHTML = '<div class="alert ' + alertClass + '"><strong>' + alertType + ':</strong> ' + alertText + '</div>';
-		
-		
-	}
-	
-
 	function parseCrimes (crimeData, element, membersList) {
 		
-		var table = '<div class="col-sm-12 badge-secondary" ><b>Crime Details</b></div>';
+		var table = '<div class="col-sm-12 badge-primary" ><b>Crime Details</b></div>';
 		table = table + '<table class="table table-hover"><thead><tr>'
-				  + '<th>Result</th>'
 				  + '<th>Date</th>'
-				  + '<th>Crime Type</th>'
 				  + '<th>Participants</th>'
-				  + '<th>Money Gained<br/>per member</th>'
+				  + '<th>Crime Type</th>'
+				  + '<th>Result</th>'
+				  + '<th>Money Gained<br/>'
 				  + '<th>Respect Gained</th>'
 				  + '</tr></thead><tbody>';
 				  
-		var memberMap = {};
+		var memberMoney = {};
+		var memberSuccess = {};
+		var memberFailed = {};
+		var factionMoney = 0;
+		var factionSuccess = 0;
+		var factionFailed = 0;
+		var totalRespect = 0;
+		var totalMoney = 0;
 		var today = new Date();
+		var badgeSuccess = 'badge-dark';
+		var badgeFailed = 'badge-dark';
 		
 		if (document.getElementById('current').checked) {
 			var currentMonth = today.getMonth() + 1;
@@ -78,7 +73,7 @@
 			var currentMonth = today.getMonth() - 1;
 			if (currentMonth < 0) { currentMonth = 11; }
 		}
-		//console.log(currentMonth);
+		
 		for( var id in crimeData ){
 			var crime = crimeData[id];
 			if (crime.crime_id === 8) { //8 = PA
@@ -87,13 +82,17 @@
 				if (crime.initiated === 1 && ts.getMonth()+1 === currentMonth) {
 					
 					var crimeResult = '';
+					var failed = 0;
+					var success = 0;
 					var participants = '';
 					var tmp = '';
 
 					if (crime.success === 0) {
 						crimeResult = '<span class="badge badge-pill badge-danger">Failed</span>';
+						failed = 1;
 					} else {
 						crimeResult = '<span class="badge badge-pill badge-success">Success</span>';
+						success = 1;
 					}
 					
 					crime.participants.forEach(obj => {
@@ -103,12 +102,15 @@
 							var memberName =  '';
 							if (JSON.stringify(membersList).indexOf(memberID) != -1) {
 								memberName = membersList[memberID].name;
-								if (memberName in memberMap) {
-									memberMap[memberName] = memberMap[memberName] + (crime.money_gain / 5);
+								if (memberName in memberMoney) {
+									memberMoney[memberName] = memberMoney[memberName] + (crime.money_gain / 5);
+									memberSuccess[memberName] = memberSuccess[memberName] +success;
+									memberFailed[memberName] = memberFailed[memberName] + failed;
 								} else {
-									memberMap[memberName] = (crime.money_gain / 5);
+									memberMoney[memberName] = (crime.money_gain / 5);
+									memberSuccess[memberName] = success;
+									memberFailed[memberName] = failed;
 								}
-								
 							} else {
 								memberName = memberID;
 							}
@@ -121,13 +123,20 @@
 							}
 						});
 					});
-						
+					
+					factionMoney = factionMoney + (crime.money_gain / 5);
+					factionSuccess = factionSuccess + success;
+					factionFailed = factionFailed + failed;
+					totalRespect = totalRespect + crime.respect_gain;
+					totalMoney = totalMoney + crime.money_gain;
+					
+					var formatted_date = ts.getFullYear() + "-" + (ts.getMonth() + 1) + "-" + ts.getDate() + " " + ts.getHours() + ":" + ts.getMinutes() + ":" + ts.getSeconds()
 					
 					table = table + '<tr>'
-							+'<td>' + crimeResult + '</td>'
-							+'<td>' + ts.toISOString() + '</td>'
-							+'<td>' + crime.crime_name + '</td>'
+							+'<td>' + formatted_date + '</td>'
 							+'<td>' + participants + '</td>'
+							+'<td>' + crime.crime_name + '</td>'
+							+'<td>' + crimeResult + '</td>'
 							+'<td>$' + crime.money_gain.toLocaleString('en-US') + '</td>'
 							+'<td>' + crime.respect_gain + '</td>'
 							+'</tr>';
@@ -135,41 +144,85 @@
 			}
 		}
 		
+		if (factionFailed > 0) {badgeFailed = 'badge-danger';}
+		if (factionSuccess > 0) {badgeSuccess = 'badge-success';}
+		
+		table = table + '<tr class="table-dark">' 
+						+'<td colspan = "3">Faction totals</td>'
+						+'<td>' 
+							+ '<span class="badge badge-pill '+badgeFailed+'">'+ factionFailed + '</span>-'
+							+ '<span class="badge badge-pill '+badgeSuccess+'">'+ factionSuccess + '</span>'
+						+'</td>'
+						+'<td>$' + totalMoney.toLocaleString('en-US') + '</td>'
+						+'<td>' + totalRespect + '</td>'
+						+'</tr>';
+		
 		table = table + '</tbody></table>';
 		document.getElementById(element).innerHTML = table;
 		
 		
-		var summary = '<div class="col-sm-12 badge-secondary" ><b>Results for ' + monthToText(currentMonth) + '</b></div>';
-		summary = summary + '<table class="table table-hover" style="max-width: 50%;"><thead><tr>'
+		var summary = '<div class="col-sm-12 badge-primary" ><b>Results for ' + monthToText(currentMonth) + '</b></div>';
+		summary = summary + '<table class="table table-hover"><thead><tr>'
 				  + '<th>Name</th>'
-				  + '<th>Money earned</th>'
+				  + '<th>Money earned (<sup>1</sup>/<sub>5</sub>th of result)</th>'
+				  + '<th>Success Rate</th>'
 				  + '</tr></thead><tbody>';
 				  
-		memberMap = sortObj(memberMap);
-		for (var name in memberMap) {
+		memberMoney = sortObj(memberMoney);
+		
+		for (var name in memberMoney) {
+			if (memberFailed[name] > 0) {badgeFailed = 'badge-danger';} else {badgeFailed = 'badge-dark';}
+			if (memberSuccess[name] > 0) {badgeSuccess = 'badge-success';} else {badgeSuccess = 'badge-dark';}
 			summary = summary + '<tr>' 
 						+'<td>' + name + '</td>'
-						+'<td>' +' $' + memberMap[name].toLocaleString('en-US') + '</td>'
+						+'<td>' +' $' + memberMoney[name].toLocaleString('en-US') + '</td>'
+						+'<td>' 
+							+ '<span class="badge badge-pill '+badgeFailed+'">'+ memberFailed[name] + '</span>-'
+							+ '<span class="badge badge-pill '+badgeSuccess+'">' + memberSuccess[name] + '</span>'
+						+ '</td>'
 						+'</tr>';
 			
 		}
+		badgeSuccess = 'badge-dark';
+		badgeFailed = 'badge-dark';
+		if (factionFailed > 0) {badgeFailed = 'badge-danger';}
+		if (factionSuccess > 0) {badgeSuccess = 'badge-success';}
+		summary = summary + '<tr class="table-dark">' 
+						+'<td>Faction totals</td>'
+						+'<td>' +' $' + factionMoney.toLocaleString('en-US') + '</td>'
+						+'<td>' 
+							+ '<span class="badge badge-pill '+badgeFailed+'">'+ factionFailed + '</span>-'
+							+ '<span class="badge badge-pill '+badgeSuccess+'">'+ factionSuccess + '</span>'
+						+ '</td>'
+						+'</tr>';
 		summary = summary + '</tbody></table>';
 		
 		document.getElementById('summary').innerHTML = summary;
 	}
+	
+	
+	function printAlert(alertType, alertText) {
+		var alertClass;
+		if (alertType === 'Error') { alertClass = 'alert-danger' };
+		if (alertType === 'Success') { alertClass = 'alert-success' };
+		if (alertType === 'Info') { alertClass = 'alert-info' };
+		if (alertType === 'Warning') { alertClass = 'alert-warning' };
+		if (alertType === '#chedded') { alertClass = 'alert-danger' };
+		
+		document.getElementById('alert').innerHTML = '<div class="alert ' + alertClass + '"><strong>' + alertType + ':</strong> ' + alertText + '</div>';
+	}
 
+	function sortObj(obj) {
+	  return Object.keys(obj).sort().reduce(function (result, key) {
+		result[key] = obj[key];
+		return result;
+	  }, {});
+	}
 
-function sortObj(obj) {
-  return Object.keys(obj).sort().reduce(function (result, key) {
-    result[key] = obj[key];
-    return result;
-  }, {});
-}
+	function monthToText(month) {
+		const monthNames = ["January", "February", "March", "April", "May", "June",
+	  "July", "August", "September", "October", "November", "December"];
+	  
+	  return monthNames[month - 1];
 
-function monthToText(month) {
-	const monthNames = ["January", "February", "March", "April", "May", "June",
-  "July", "August", "September", "October", "November", "December"];
-  
-  return monthNames[month - 1];
-
-}
+	}
